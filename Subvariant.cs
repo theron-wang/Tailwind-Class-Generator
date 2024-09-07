9300950947f1b900462ec1d01f7c831606321e4d
Theron Wang <doritocat2015@gmail.com>
2024-09-06 22:07:32 -0400

    Add project files.

---
+++ Subvariant.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AllTailwindClassesGenerator;
+
+public class Subvariant
+{
+    [JsonPropertyName("ss")]
+    public required string Stem { get; set; }
+    [JsonPropertyName("v")]
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public List<string>? Variants { get; set; }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    safelist: [
+        {
+            pattern: /./, // the "." means "everything"
+        },
+    ],
+    theme: {}
+}
+
